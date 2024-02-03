bb6586f8b4430d35f07d0e16db7e95d6fd93ec8c
pablocastro <pablocastro079@gmail.com>
2024-02-02 19:37:17 -0600

    try to fix karma unit test

--- karma.conf.js
+++ karma.conf.js
@@ -11,15 +11,12 @@
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        // Configuration options for Jasmine
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // Leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true // Removes the duplicated traces
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/'),
@@ -34,9 +31,16 @@
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    // Define custom launchers
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-translate', '--disable-extensions', '--disable-dev-shm-usage']
+      }
+    },
+    // Use the custom launcher
+    browsers: ['ChromeHeadlessCI'],
     singleRun: false,
     restartOnFileChange: true
   });
 };
-
