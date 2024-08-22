d9bcaad257fc4190c0fd7222999c83c4bd99dbd9
metal079 <pablocastro079@gmail.com>
2024-08-21 22:00:22 -0500

    Added SwUpdate for automatic update grabbing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private swUpdate: SwUpdate) {
+    this.checkForUpdates();
+  }
 
+  checkForUpdates() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.checkForUpdate().then(() => {
+        console.log("Checked for updates");
+      });
+
+      this.swUpdate.versionUpdates.subscribe(event => {
+        if (event.type === 'VERSION_READY') {
+          if (confirm("New version available. Load New Version?")) {
+            window.location.reload();
+          }
+        }
+      });
+    }
+  }
 }
