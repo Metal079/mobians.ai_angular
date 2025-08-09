f694cab83824fb30a50f5df9bd2f963325bac74b
pablocastro <pablocastro079@gmail.com>
2025-08-09 02:03:54 -0500

    reverted accidently setting dev api url to prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     discordRedirectUri: 'http://localhost:4200/',  // Replace with your development redirect URI
-    apiBaseUrl: 'https://api.mobians.ai',
+    apiBaseUrl: 'http://localhost:8001',
     isDevJob: true,
   };
   
